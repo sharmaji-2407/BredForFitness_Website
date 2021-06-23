cfb58cb3045fb9d0195cba40394a58e6f90094db
sharmaji-2407 <prakharksharma@gmail.com>
2021-06-23 16:57:36 +0530

    css more

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,16 @@
 import React,{useState} from 'react'
+import {Route} from 'react-router-dom'
 import Form2 from "./Form2"
 
 const Form =() =>{
+    const [Active, setActive] = useState("One");
     return(
+        
         <>
             <h1>Form1</h1>
+            <Route render={({ history}) => (
+            <button className="form_btn btn-success" onClick={} >Home</button>
+            )} />
             
         </>
     );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import {Route} from 'react-router-dom'
 import "./Home.css"
 import Logo from "./banner2.png";
+import Form from './Form';
 
 const Home =() =>{
     return(
         <>
             <img className="logo" src={Logo} alt="Bred For Fitness" />
             
-            <div className="Container">
+            <div className="Container col-6 col-lg-4">
                 <p className="welcome_text">
                     Workout Program Made Just For You
                 </p>
             </div>
-            <button className="form_btn btn-success">Form</button>
+            <Route render={({ history}) => (
+            <button className="form_btn btn-success" onClick={()=>{history.push('/form')}} >Form</button>
+            )} />
 
             
         </>
